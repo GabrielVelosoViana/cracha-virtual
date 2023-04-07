eed3e7d28662248e571220286727aaa539af3cfe
Gabriel Veloso Viana <93293329+GabrielVelosoViana@users.noreply.github.com>
2023-04-07 15:22:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'GabrielVelosoViana',
+  youtube: '',
+  facebook: 'GabrielVeloso123',
+  instagram: '_gabrielveloso',
+  twitter: '_gabrielveloso'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userLogin.textContent = data.login
+      userImage.src = data.avatar_url
+    })
+}
+
+getGitHubProfileInfos()
